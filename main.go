3d4fb4d0f7161a1e5480e2878699437460230dae
needrima <oyebodeamirdeen@gmail.com>
2021-09-29 22:27:05 -0700

    commit

--- main.go
+++ main.go
@@ -59,41 +59,52 @@ func main() {
 func CrawlNews(w http.ResponseWriter, r *http.Request) {
 	collector := colly.NewCollector()
 
-	collector.OnError(func(_ *colly.Response, err error) {
-		log.Println("Collector error: ", err.Error())
-		http.Error(w, "Something went wrong", 500)
-		return
-	})
+	// collector.OnError(func(_ *colly.Response, err error) {
+	// 	log.Println("Collector error: ", err.Error())
+	// 	http.Error(w, "Something went wrong", 500)
+	// 	return
+	// })
 
-	collector.OnResponse(func(r *colly.Response) {
-		log.Println("Visiting: %s\t StatusCode:", r.Request.URL, r.StatusCode)
-	})
+	// collector.OnResponse(func(r *colly.Response) {
+	// 	log.Println("Visiting: %s\t StatusCode:", r.Request.URL, r.StatusCode)
+	// })
 
 	punch := getNews(".list-item article", ".entry-title a", ".entry-title a", ".entry-meta .meta-time span", "https://www.punchng.com", collector)
 	punch = filterNews(punch)
 
+	collector = colly.NewCollector()
 	theGuardian := getNews(".row-3 .cell", "a .headline span", "a", "a .meta span", "https://www.guardian.ng", collector)
 	theGuardian = filterNews(theGuardian)
 
+	collector = colly.NewCollector()
 	theSun := getNews("article .jeg_postblock_content", "h3 a", "h3 a", ".jeg_post_meta .jeg_meta_date a", "https://www.sunnewsonline.com/", collector)
 	theSun = filterNews(theSun)
 
+	collector = colly.NewCollector()
 	premiumTimes := getNews("article .jeg_postblock_content", "h3 a", "h3 a", ".jeg_post_meta .jeg_meta_date a", "https://www.premiumtimesng.com/", collector)
 	premiumTimes = filterNews(premiumTimes)
 
+	collector = colly.NewCollector()
 	aljazeera := getNews("article .gc__content", ".gc__header-wrap .gc__title a span", ".gc__header-wrap .gc__title a", ".gc__footer .gc__meta .gc__date .gc__date__date .date-simple", "https://www.aljazeera.com/where/nigeria/", collector)
 
+	collector = colly.NewCollector()
 	saharaNews := getNews(".block-module-content", ".block-module-content-header span a", ".block-module-content-header span a", ".block-module-content-footer .block-module-content-footer-item-date", "https://www.saharareporters.com/", collector)
 	saharaNews = filterNews(saharaNews)
 
+	collector = colly.NewCollector()
 	dailyTrust := getNews(".list_body__19fyx", "a", "a", ".list_category__1sVu4 span.list_time__1UhFn", "https://dailytrust.com", collector) // prefix media link with https://www.dailytrust.com
 	dailyTrust = dailyTrust[9:]
 
+	collector = colly.NewCollector()
 	dailypost := getNews(".mvp-blog-story-wrap", "a .mvp-blog-story-in .mvp-blog-story-text h2", "a", "a .mvp-blog-story-in .mvp-blog-story-text .mvp-cat-date-wrap .mvp-cd-date", "https://dailypost.ng/headlines/", collector)
 
+	collector = colly.NewCollector()
 	skysports := getNews(".sdc-site-tile__body-main", ".sdc-site-tile__headline a span", ".sdc-site-tile__headline a", ".sdc-site-tile__info .sdc-site-tile__tag a ", "https://www.skysports.com/", collector) //prefix link with https://www.skysports.com/
 
+	collector = colly.NewCollector()
 	completesports := getNews(".td", ".item-title a span", ".item-title a", ".meta-item-date a span", "https://www.completesports.com/", collector)
+	
+	collector = colly.NewCollector()
 	completesports2 := getNews(".item-sub", ".item-title a", ".item-title a", ".meta-items .meta-item-date span", "https://www.completesports.com/", collector)
 	completesports2 = filterNews(completesports2)
 
